a1e7a732917ccb91002bedd930d83bbdf70b259e
Mozok Evgen <mozokevgen@gmail.com>
2023-09-30 13:48:25 +0300

    Bassc data for bags and items

--- main/model/bag.lua
+++ main/model/bag.lua
@@ -1,4 +1,52 @@
--- Put functions in this file to use them in several other scripts.
--- To get access to the functions, you need to put:
--- require "my_directory.my_file"
--- in any script using the functions.
+local used_bags = {}
+
+local M = {
+	data = {
+		[1] = {
+			index = 1,
+			name = "Framebag Small",
+			volume = 4,
+			require_item = false,
+			cost = 100,
+			image = hash("framebag2"),
+			position = "frame",
+			is_used = false
+		},
+		[2] = {
+			index = 2,
+			name = "Rear Mini-Panniers",
+			volume = 30,
+			require_item = 1,
+			cost = 200,
+			image = hash("rackbag"),
+			position = "rear-rack",
+			is_used = false
+		},
+	}
+}
+
+function M.add_used(index)
+	table.insert(used_bags, index)
+	M.data[index].is_used = false
+end
+
+function M.remove_used(index)
+	for key, value in ipairs(used_bags) do
+		if value == index then
+			M.data[index].is_used = false
+			table.remove(used_bags, key)
+		end
+	end
+end
+
+function M.get_used()
+	local result = {}
+	
+	for _, index in ipairs(used_bags) do
+		table.insert(result, M.data[index])
+	end
+
+	return result
+end
+
+return M
--- main/model/item.lua
+++ main/model/item.lua
@@ -1,4 +1,40 @@
--- Put functions in this file to use them in several other scripts.
--- To get access to the functions, you need to put:
--- require "my_directory.my_file"
--- in any script using the functions.
+local used_items = {}
+
+local M = {
+	data = {
+		[1] = {
+			index = 1,
+			name = "rack",
+			cost = 20,
+			callback = function() end,
+			is_used = false
+		}
+	}
+}
+
+function M.add_used(index)
+	table.insert(used_items, index)
+	M.data[index].is_used = false
+end
+
+function M.remove_used(index)
+	for key, value in ipairs(used_items) do
+		if value == index then
+			M.data[index].is_used = false
+			table.remove(used_items, key)
+		end
+	end
+end
+
+function M.get_used()
+	local result = {}
+
+	for _, index in ipairs(used_items) do
+		table.insert(result, M.data[index])
+	end
+
+	return result
+end
+
+
+return M
