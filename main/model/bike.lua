a98dff3a3e6811b379c93f3aaf5b921aaf0a489c
Mozok Evgen <mozokevgen@gmail.com>
2023-09-30 13:23:07 +0300

    Show bike info

--- main/model/bike.lua
+++ main/model/bike.lua
@@ -1,4 +1,21 @@
--- Put functions in this file to use them in several other scripts.
--- To get access to the functions, you need to put:
--- require "my_directory.my_file"
--- in any script using the functions.
+local default_values = {
+	max_volume = 0,
+	volume = 0,
+	weight = 15,
+	cost = 0
+}
+
+local M = {}
+
+M.max_volume = 0
+M.volume = 0
+M.weight = 15
+M.cost = 0
+
+function M.reset_default_values()
+	for key, value in pairs(default_values) do
+		M[key] = value
+	end
+end
+
+return M
