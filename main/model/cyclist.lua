a87ca082ff3cd7b0d2d4b5a2308b732adc3c27e6
Mozok Evgen <mozokevgen@gmail.com>
2023-09-30 12:21:25 +0300

    Open cyclist info on start

--- main/model/cyclist.lua
+++ main/model/cyclist.lua
@@ -1,4 +1,37 @@
--- Put functions in this file to use them in several other scripts.
--- To get access to the functions, you need to put:
--- require "my_directory.my_file"
--- in any script using the functions.
+--[[
+Describe cyclist types.
+Select current cyclist.
+Get current cyclist.
+--]]
+
+local CYCLISTS = {
+	[1] = {
+		name = "Dave",
+		description = "Hello Mr. CyclingCerviceExpertSir :)\nI want to try some cycling\nfor the first time.",
+		experience = 0,
+		money = 0,
+		weight_limit = 0,
+		ride_callback = function() end,
+	},
+}
+
+local cyclist_index = 1
+
+local M = {}
+
+---@return table
+function M.get_cyclist()
+	return CYCLISTS[cyclist_index]
+end
+
+---@return boolean
+function M.next_cyclist()
+	if cyclist_index + 1 > #CYCLISTS then
+		return false
+	end
+	
+	cyclist_index = cyclist_index + 1
+	return true
+end
+
+return M
