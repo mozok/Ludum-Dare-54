d867a4cc2039b6d279fbbe08981559b1a4e9e1f6
Mozok Evgen <mozokevgen@gmail.com>
2023-09-30 18:27:58 +0300

    Add route component and selection

---
+++ gui/template/route_component.lua
@@ -0,0 +1,83 @@
+--- For component interest functions
+--- see https://github.com/Insality/druid/blob/develop/docs_md/02-creating_custom_components.md
+--- Require this component in you gui file:
+--- local RouteComponent = require("gui.template.route_component")
+--- And create this component via:
+--- self.route_component = self.druid:new(RouteComponent, template, nodes)
+
+local Event = require("druid.event")
+local component = require("druid.component")
+
+---@class route_component: druid.base_component
+---@field root node
+---@field text_name druid.text
+---@field text_distance druid.text
+---@field text_description druid.text
+---@field druid druid_instance
+local RouteComponent = component.create("route_component")
+
+local SCHEME = {
+	ROOT = "root",
+	PANEL_FRAME = "panel_frame",
+	ICON_FRAME = "icon_frame",
+	ICON = "icon",
+	TEXT_NAME = "text_name",
+	TEXT_DISTANCE = "text_distance",
+	TEXT_DESCRIPTION = "text_description",
+	PANEL_CHECKBOX = "panel_checkbox",
+	ICON_CHECKBOX = "icon_checkbox"
+}
+
+
+---@param template string
+---@param nodes table<hash, node>
+function RouteComponent:init(template, nodes)
+	self:set_template(template)
+	self:set_nodes(nodes)
+	self.druid = self:get_druid()
+
+	self.root = self:get_node(SCHEME.ROOT)
+	self.text_name = self.druid:new_text(SCHEME.TEXT_NAME)
+	self.text_distance = self.druid:new_text(SCHEME.TEXT_DISTANCE)
+	self.text_description = self.druid:new_text(SCHEME.TEXT_DESCRIPTION)
+	self.checkbox = self:get_node(SCHEME.ICON_CHECKBOX)
+
+	self.button = self.druid:new_button(self.root, self._on_click)
+
+	self.on_click = Event()
+end
+
+function RouteComponent:set_data(data)
+	self._data = data
+
+	-- gui.play_flipbook(self.icon, data.image)
+
+	self.text_name:set_to(data.name)
+	self.text_distance:set_to("Distance: " .. data.distance)
+	self.text_description:set_to(data.description)
+
+	self:set_checked(self._data.is_selected)
+end
+
+function RouteComponent:get_data()
+	return self._data
+end
+
+function RouteComponent:set_checked(state)
+	self._data.is_checked = state
+	gui.set_enabled(self.checkbox, state)
+end
+
+function RouteComponent:set_click_zone(node)
+	self.button:set_click_zone(node)
+end
+
+function RouteComponent:on_remove()
+	self.on_click:clear()
+end
+
+function RouteComponent:_on_click()
+	self.on_click:trigger(self)
+end
+
+return RouteComponent
--- main/model/route.lua
+++ main/model/route.lua
@@ -1,4 +1,33 @@
--- Put functions in this file to use them in several other scripts.
--- To get access to the functions, you need to put:
--- require "my_directory.my_file"
--- in any script using the functions.
+local M = {}
+
+M.data = {
+	[1] = {
+		index = 1,
+		name = "To the local River",
+		description = "",
+		distance = 40,
+		callback = function() end,
+		is_selected = false
+	},
+	[2] = {
+		index = 2,
+		name = "Race",
+		description = "",
+		distance = 200,
+		callback = function() end,
+		is_selected = false
+	}
+}
+
+M.selected = 0
+
+function M.select_route(index)
+	for i, route in ipairs(M.data) do
+		route.is_selected = false
+	end
+
+	M.selected = index
+	M.data[index].is_selected = true
+end
+
+return M
